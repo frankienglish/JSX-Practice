cf80cfc6f6012a76bbe036609c91cd8da5f2fedc
frankienglish <frankienglish0416@gmail.com>
2021-01-11 19:58:55 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// parameters: WHAT TO SHOW, WHERE TO SHOW IT, WHEN IT IS COMPLETED (OPTIONAL)
+ReactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+    <p>This is a paragrahp</p>
+  </div>,
+  document.getElementById("root")
+);
